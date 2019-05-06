afcd3778c99d2114785271a1728edb01de5852ea
Brian Kang <brianhkang1@gmail.com>
2019-05-06 00:19:00 -0400

    cleaned up front page. mobile layout bit cleaner, uses textloop

--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -2,30 +2,89 @@ import React from 'react';
 import styles from './HomePage.module.css'
 import Header from '../Header';
 import ReactTypingEffect from 'react-typing-effect';
+import TextLoop from "react-text-loop";
 
-class HomePage extends React.Component{
+type State = {
+  color: string;
+}
+
+class HomePage extends React.Component<{}, State>{
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      color: 'white'
+    }
+  }
+  
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenScrollEvent)
+  }
+
+  listenScrollEvent = e => {
+    // mobile scroll event
+    if(window.matchMedia("(max-width: 600px)")){
+      if (window.scrollY > 765 && window.scrollY < 1530) {
+        this.setState({color: 'rgb(69, 158, 172)'})
+      } else if (window.scrollY > 1530 && window.scrollY < 2295 ) {
+        this.setState({color: 'white'})
+      } else {
+        this.setState({color: 'white'})
+      }
+    } 
+    // laptop scroll event
+    else {
+      if (window.scrollY > 765 && window.scrollY < 1530) {
+        this.setState({color: 'rgb(69, 158, 172)'})
+      } else if (window.scrollY > 1530 && window.scrollY < 2295 ) {
+        this.setState({color: 'white'})
+      } else {
+        this.setState({color: 'white'})
+      }
+    }
+  }
+
+  renderFeatureTitle = () => {
+    if(window.matchMedia("(max-width: 600px)").matches){
+      return (
+      <>
+        <div>We are</div>
+        <TextLoop children={["leaders.", "difference-makers.", "avengers."]}/>
+      </>
+      ) 
+    }
+    else if (window.matchMedia("(min-width: 601px)").matches){
+      return (
+      <>
+        <ReactTypingEffect
+          staticText='We are'
+          eraseDelay={2500}
+          speed={100}
+          text={["leaders.", "difference-makers.", "avengers." ]}
+        />
+      </>
+      )
+    }
+  }
+
   render(){
     return (
-      <>
+      <div style={{backgroundColor: this.state.color, transition: 'background-color 0.75s'}}>
         <div className={styles.FeaturePictureContainer}>
           <Header/>
-
           <div className={styles.typeWriter}>
-            <ReactTypingEffect
-              staticText='We are'
-              eraseDelay={2500}
-              speed={100}
-              text={["leaders.", "difference-makers.", "avengers." ]}
-            />
-          </div>
+            {this.renderFeatureTitle()}
+          </div>  
         </div>
+
         <div className={styles.container2}>
           container 2
         </div>
+
         <div className={styles.container3}>
           container 3
         </div>
-      </>
+      </div>
     );
   }
 }
