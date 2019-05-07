f9f2f4b9e9d3fb282b2759cef1f34db21d513e59
Brian Kang <brianhkang1@gmail.com>
2019-05-07 01:48:29 -0400

    WIP: mobile menu

---
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './MobileMenu.module.css'
+
+class MobileMenu extends React.Component{
+  render(){
+    return(
+      <div className={styles.mobileMenu}>
+      
+      </div>
+    )
+  }
+}
+
+export default MobileMenu
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <BrowserRouter>
