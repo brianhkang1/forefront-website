96956f29d4cff79bd3864e79904bda26ae8006be
Brian Kang <brianhkang1@gmail.com>
2019-10-29 00:48:10 -0400

    revamp mission statement and who we are

--- src/containers/OurMissionContainer/OurMissionContainer.tsx
+++ src/containers/OurMissionContainer/OurMissionContainer.tsx
@@ -1,63 +1,47 @@
 import React from 'react';
 import styles from './OurMissionContainer.module.css';
+import { classNames } from '../../util';
 import Title from '../../components/Title';
 
 const OurMissionContainer = () => {
   return(
     <div className={styles.root}>
-      <div style={{paddingTop: '2rem'}}>
-        <Title
-          title='Our Mission'
-        />
+
+      <div className={styles.textContainer}>
+        <div className={styles.definitionContainer}>
+          <div className={styles.forefront}>FOREFRONT</div>
+          <div className={styles.grayText}>
+            <span>/ˈfɔrˌfrʌnt/</span>
+            <span className={styles.italic}>Noun</span>
+          </div>
+          <div className={classNames(styles.grayText, styles.definition)}>
+            A non-profit organization that facilitates sustainable, 
+            local projects in India and
+            around the world to increase access to clean water,
+            education, health care, and leadership training.
+          </div>
+        </div>
+        <div className={styles.missionsContainer}>
+          <div className={styles.missionTitle}>OUR MISSION</div>
+          <div className={styles.mission}>Enable every person</div>
+          <div className={styles.mission}>Equip leaders</div>
+          <div className={styles.mission}>Establish sustainable communities</div>
+        </div>
       </div>
-
-      <div className={styles.descriptionContainer}>
-        <span className={styles.description}>
-          Enable every person
-        </span>
-
-        <span className={styles.forwardSlash}> / </span>
-
-        <span className={styles.description}>
-          Equip leaders
-        </span>
-
-        <span className={styles.forwardSlash}> / </span>
-
-        <span className={styles.description}>
-          Establish self-sustaining communities
-        </span>
-      </div>
-
-      <div className={styles.mainContainer}>
+     
         <div className={styles.video}>
           <iframe
             title='home page video'
             style={{
-              width: '35vw',
-              height: '99.75%',
-              borderRadius: '20px 0 0 20px'
-              // position: "absolute",
-              // top: 0,
-              // left: 0,
-              // border: "thin solid transparent",
+              width: '60vw',
+              height: '50vh',
+              borderRadius: '10px'
             }}
             src={`https://www.youtube.com/embed/VVfv3N4yLyM`}
             frameBorder="0"
             allow="fullscreen; accelerometer; encrypted-media; gyroscope; picture-in-picture" 
           />
         </div>
-
-        <div className={styles.subHeaderContainer}>
-          <div className={styles.whoWeAre}>Who We Are</div>
-          <div>
-            Forefront is a non-profit charity organization randomised words which don't look even slightly believable. 
-            If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
-          </div>
-        </div>
-      </div>
-
-
     </div>
   )
 }
