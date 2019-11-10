eedf9bf7e00975d007e784b86b5af6931ec90ec2
Brian Kang <brianhkang1@gmail.com>
2019-11-09 21:46:25 -0500

    email subscription finally working. adding some more logic

--- src/components/EmailForm/EmailForm.tsx
+++ src/components/EmailForm/EmailForm.tsx
@@ -7,6 +7,7 @@ interface State {
   firstName: string;
   lastName: string;
   email: string;
+  isModalOpen: boolean;
 }
 
 const encode = (data) => {
@@ -22,31 +23,82 @@ class EmailForm extends React.Component<{}, State> {
     firstName: "",
     lastName: "",
     email: "",
+    isModalOpen: false
    }
  }
 
   handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    this.setState({ 
-      [e.currentTarget.name]: e.currentTarget.value
-    } as Pick<State, keyof State>);
+    if(e.currentTarget.name === 'firstName'){
+      return this.setState({ 
+        firstName: e.currentTarget.value
+      });
+    }
+    if(e.currentTarget.name === 'lastName'){
+      return this.setState({ 
+        lastName: e.currentTarget.value
+      });
+    }
+    if(e.currentTarget.name === 'email'){
+      return this.setState({ 
+        email: e.currentTarget.value
+      });
+    }
   };
 
   handleSubmit = (e: React.FormEvent) => {
+    const { firstName, lastName, email } = this.state;
+
+    const body = {
+      firstName, 
+      lastName, 
+      email
+    }
+
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "Email subscription", ...this.state })
+      body: encode({ "form-name": "Email subscription", ...body })
     })
+      .then((res) => {
+        if(res.status === 200){
+          this.closeModal()
+          alert("You are successfully subscribed for Forefront emails")
+        }
+        if(res.status === 404){
+          alert("Unable to subscribe. Please try again later")
+        }
+      })
       .catch(error => alert(error));
 
     e.preventDefault();
   };
 
+  openModal = () => {
+    this.setState({
+      isModalOpen: true
+    })
+  }
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false
+    })
+  }
+
  render(){
-   const { firstName, lastName, email } = this.state;
+   const { firstName, lastName, email, isModalOpen } = this.state;
 
    return (
-    <Modal size="small" trigger={<Button size="large">Join Our List</Button>}>
+    <Modal 
+      size="small" 
+      open={isModalOpen}
+      onClose={this.closeModal}
+      trigger={
+        <Button size="large" onClick={this.openModal}>
+          Join Our List
+        </Button>
+      }
+    >
       <div className={styles.root}>
         <div className={styles.titleContainer}>
           <div className={styles.title}>GET EMAIL UPDATES</div>
@@ -56,13 +108,13 @@ class EmailForm extends React.Component<{}, State> {
         <form name="Email subscription" method="post" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={this.handleSubmit}>
           <input type="hidden" name="form-name" value="Email subscription" />
           <div className={styles.inputContainer}>
-            <input className={styles.input} type="text" name="firstName" placeholder="First Name" value={firstName} onChange={this.handleChange} />   
+            <input required className={styles.input} type="text" name="firstName" placeholder="First Name" value={firstName} onChange={this.handleChange} />   
           </div>
           <div className={styles.inputContainer}>
-            <input className={styles.input} type="text" name="lastName" placeholder="Last Name" value={lastName} onChange={this.handleChange} />    
+            <input required className={styles.input} type="text" name="lastName" placeholder="Last Name" value={lastName} onChange={this.handleChange} />    
           </div>
           <div className={styles.inputContainer}>
-            <input  className={styles.input}type="email" name="email" placeholder="Email Address" value={email} onChange={this.handleChange} />
+            <input required className={styles.input} type="email" name="email" placeholder="Email Address" value={email} onChange={this.handleChange} />
           </div>
           <div className={styles.buttonContainer}>
             <Button 
