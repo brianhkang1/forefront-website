4f8d882c3a4d1011c1774de1acb39a24a0940f5b
Brian Kang <brianhkang1@gmail.com>
2020-06-30 22:27:47 -0400

    googleAnalytics setup pt2

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 import ScrollToTop from './components/ScrollToTop';
 import { createBrowserHistory } from 'history';
@@ -14,14 +14,15 @@ const history = createBrowserHistory();
 // Initialize google analytics
 if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID || '');
-  history.listen(location => {
-    ReactGA.set({ page: location.pathname }); // Update the user's current page
-    ReactGA.pageview(location.pathname); // Record a pageview for the given page
-  });
 }
 
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
 ReactDOM.render(
-  <Router>
+  <Router history={history}>
     <ScrollToTop />
     <App />
   </Router>,
