a559d769118ea6c25ec7e39c0e38485455a9cc24
Brian Kang <30248266+brianhkang1@users.noreply.github.com>
2022-12-03 11:11:56 -0500

    update footer copyright

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -18,62 +18,54 @@ class Footer extends React.Component {
             <a
               className={styles.whiteIcon}
               href={'https://www.facebook.com/goforefront'}
-              target="_blank"
-              rel="noopener noreferrer"
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <Icon name="facebook f" size="large" />
+              <Icon name='facebook f' size='large' />
             </a>
             <a
               className={styles.whiteIcon}
               href={'https://www.instagram.com/goforefront'}
-              target="_blank"
-              rel="noopener noreferrer"
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <Icon name="instagram" size="large" />
+              <Icon name='instagram' size='large' />
             </a>
             <a
               className={styles.whiteIcon}
               href={'http://smile.amazon.com/ch/47-3373120'}
-              target="_blank"
-              rel="noopener noreferrer"
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <Icon name="amazon" size="large" />
+              <Icon name='amazon' size='large' />
             </a>
             <a
               className={styles.whiteIcon}
               href={'https://twitter.com/goforefront'}
-              target="_blank"
-              rel="noopener noreferrer"
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <Icon name="twitter" size="large" />
+              <Icon name='twitter' size='large' />
             </a>
             <a
               className={styles.whiteIcon}
               href={'https://blog.goforefront.org/'}
-              target="_blank"
-              rel="noopener noreferrer"
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <img
-                className={styles.exposureIcon}
-                src={ExposureIcon}
-                width="28px"
-                height="28px"
-                alt="Exposure icon"
-              />
+              <img className={styles.exposureIcon} src={ExposureIcon} width='28px' height='28px' alt='Exposure icon' />
             </a>
           </div>
         </div>
 
         <div className={styles.rightAlign}>
-          <div className={styles.text}>© 2021 by FOREFRONT Charity</div>
+          <div className={styles.text}>© 2023 by FOREFRONT Charity</div>
           <div className={`${styles.text} ${styles.textCenterAlign}`}>
             <div>450 Lexington Ave #1818</div>
-            <div>New York, NY, 10017</div>             
+            <div>New York, NY, 10017</div>
           </div>
           <div className={styles.text}>contact@goforefront.org</div>
-          <div className={styles.text}>
-            US 501(c)(3) Public Charity, EIN 47-3373120
-          </div>
+          <div className={styles.text}>US 501(c)(3) Public Charity, EIN 47-3373120</div>
         </div>
       </div>
     );
