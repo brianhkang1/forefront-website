b1116b4db0f4d51b752090e9df473082c0594e6b
Brian Kang <30248266+brianhkang1@users.noreply.github.com>
2024-02-16 11:14:25 -0500

    update total support stat

--- src/components/StatisticsVisual/StatisticsVisual.tsx
+++ src/components/StatisticsVisual/StatisticsVisual.tsx
@@ -21,7 +21,7 @@ const StatisticsVisual = () => {
             </div>
 
             <div className={`${styles.statsItem} wow zoomIn`}>
-              <div className={styles.numberCounterItem}>$1,524,400</div>
+              <div className={styles.numberCounterItem}>$1,571,577</div>
               <div>Total Support</div>
             </div>
           </div>
